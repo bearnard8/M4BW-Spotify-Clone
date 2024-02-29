7222045cfa6afab42c0f305320b35b621423581a
Stefano Bernardo <stefano.berna8@gmail.com>
2024-02-29 19:29:33 +0100

    index + general

--- Index/index.js
+++ Index/index.js
@@ -7,6 +7,13 @@ const welcomeArtist = "Coldplay";
 // Container card album suggeriti
 const cardContainer = document.getElementById("suggestions-container");
 
+// close the right bar-side:
+document.getElementById("rightbar-close").addEventListener("click",()=> {
+    document.getElementById("right-barside").classList.add("d-none", "d-lg-none");
+    document.getElementById("page-section").classList.remove("col-lg-8");
+    document.getElementById("page-section").classList.add("col-lg-10");
+});
+
 const albums = [];
 const rndAlbums = [];
 
